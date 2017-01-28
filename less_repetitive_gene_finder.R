2842226aa2ab2a1cd44e768dc6a8db6f9d81be1f
megara23 <megkinSW@aol.com>
2017-01-28 13:33:39 -0500

    Making the code less repetitive and easier to read

--- less_repetitive_gene_finder.R
+++ less_repetitive_gene_finder.R
@@ -1,5 +1,7 @@
 library(GEOquery)
 ## methylation datasets
+
+setwd("C:/Users/Owner/Desktop/BC-Methyl/")
 load("data/GSE37817.RData")
 load("data/GSE33510.RData")
 load("data/GSE28094.RData")
@@ -17,70 +19,20 @@ getgene = function()
   return(as.character(x))
 }
 
-getplatform = function()
-{
-  x = readline(prompt = "Enter a dataset: GSE33510, GSE37817, or GSE28094   ")
-  return (x)
-}
-
-getset = function()
-{
-  if (platform == "GSE33510"){
-    return (GSE33510.meth)
-  }
-  else if (platform == "GSE37817"){
-    return (GSE37817.methyl)
-  }
-  else if (platform == "GSE28094"){
-    return (GSE28094.meth)
-  }
-  else{
-    return ("Not a valid dataset")
-  }
-}
-
-getset2 = function() {
-  if (platform == "GSE33510" || platform == "GSE37817"){
-    return (GPLmeth)
-  }
-
-else if (platform == "GSE28094"){
-  return (GPLmeth2)
-}
-else{
-  return ("Not a valid dataset")
-}
-}
-gettumornorm = function(){
-  if (platform == "GSE33510"){
-    return (GSE33510.meth.tumor_normal)
-  }
-  else if (platform == "GSE37817"){
-    return (GSE37817.expr.tumor)
-  }
-  else if (platform == "GSE28094"){
-    return (GSE28094.methyl.tumor)
-  }
-  else{
-    return ("Not valid")
-  }
-}
-
 gene = getgene() #Enter gene
 gene = paste0("^",gene,"$") 
-platform = getplatform()
-set = getset()
-set2 = getset2()
-matching = grep(gene, set2$Symbol) #Match gene to gene name in platform data
-findprobe = set2$ID[matching] #Find probe(s) for gene
-find = match(findprobe, rownames(set)) #Match probe(s) to row(s) in patient dataset
-
-findbestprobe = function(){
+matching = grep(gene, GPLmeth$Symbol) #Match gene to gene name in platform data
+matching2 = grep(gene, GPLmeth2$Symbol)
+findprobe = GPLmeth$ID[matching] #Find probe(s) for gene
+findprobe2 = GPLmeth2$ID[matching]
+find = match(findprobe, rownames(GSE33510.meth)) #Match probe(s) to row(s) in patient dataset
+find2 = match(findprobe2, rownames(GSE33510.meth))
+findbestprobe = function(methyl, set, gettumornorm){
   newvector = c(1)
   i = 1
-  for (i in 1:length(find)) {
-    m = find[i]
-    s = split(set[m,], gettumornorm())
+  for (i in 1:length(methyl)) {
+    m = methyl[i]
+    s = split(set[m,], gettumornorm)
     boxplot(s, main = "Methylation", col = c("purple", "pink"), ylab = "Probe Expression")
     means = lapply(s, mean)
     meanchange = means[[1]] - means[[2]]
@@ -96,4 +48,6 @@ findbestprobe = function(){
   which.min(newvector) 
   min(newvector)
 }
-result = findbestprobe()
+findbestprobe(find, GSE37817.methyl, GSE37817.methyl.tumor)
+findbestprobe(find, GSE33510.meth, GSE33510.meth.tumor_normal)
+findbestprobe(find2, GSE28094.meth, GSE28094.methyl.tumor)
