9cc6722da3715e4e030a534cb0c57edb6c137a2e
megara23 <megkinSW@aol.com>
2017-03-13 11:59:21 -0400

    Remove Ureter Sample

--- getGSE33510.R
+++ getGSE33510.R
@@ -5,8 +5,10 @@ dir = getGEO("GSE33510")
 sapply(dir, annotation)
 
 GSE33510.meth.p = pData(dir[[1]])
+GSE33510.meth.p = GSE33510.meth.p[GSE33510.meth.p$source_name_ch1 != 'Normal Ureter Sample',]
 GSE33510.meth = exprs(dir[[1]])
-
+x = match(rownames(GSE33510.meth.p), colnames(GSE33510.meth))
+GSE33510.meth = GSE33510.meth[,x]
 samples.match = colnames(GSE33510.meth) == rownames(GSE33510.meth.p)
 if (!samples.match) {
   stop("need to match samples")
